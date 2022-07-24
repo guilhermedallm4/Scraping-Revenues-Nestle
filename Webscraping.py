f7aa83cfbbef83d519cdb14c2b0fbee7d31d989b
Guilherme D. Lima <54902373+guilhermedallm4@users.noreply.github.com>
2022-07-24 19:58:56 -0300

    Webscraping 4k368 receitas

--- Webscraping.py
+++ Webscraping.py
@@ -6,7 +6,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import requests
-from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
